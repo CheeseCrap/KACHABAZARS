26cfa1fb78d8b8a09e630dcdbd642ffdb78e05a3
CheeseCrap <122087526+CheeseCrap@users.noreply.github.com>
2023-01-14 09:55:26 +0000

    chore(package): update uuid to version 3.0.0

--- src/kpca.cpp
+++ src/kpca.cpp
@@ -8,3 +8,9 @@
 
 using namespace Eigen;
 using namespace std;
+
+class PCA{
+
+public:
+	PCA() : components(2), kernel_type(1), normalise(0), gamma(0.001), constant(1.0), order(2.0) {}
+	explicit PCA(MatrixXd& d) : components(2), kernel_type(1), normalise(0), gamma(0.001), constant(1.0), order(2.0) {X = d;}
