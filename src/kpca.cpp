5f5e47f4546405333e49c5cdfa859d0230c69488
CheeseCrap <122087526+CheeseCrap@users.noreply.github.com>
2023-01-14 09:02:01 +0000

    Added additional unit tests

--- src/kpca.cpp
+++ src/kpca.cpp
@@ -2,3 +2,9 @@
 // Kernel PCA using the Eigen library, by Tim Nugent 2014
 
 #include <fstream>
+#include <iostream>
+#include <Eigen/Dense>
+#include <Eigen/Eigenvalues>
+
+using namespace Eigen;
+using namespace std;
