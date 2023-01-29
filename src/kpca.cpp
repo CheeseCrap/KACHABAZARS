49c8eaea38746d695a57f05a75ee5f52bf8c3e16
CheeseCrap <122087526+CheeseCrap@users.noreply.github.com>
2023-01-29 07:45:18 +0000

    Update REJECT

--- src/kpca.cpp
+++ src/kpca.cpp
@@ -14,3 +14,8 @@ class PCA{
 public:
 	PCA() : components(2), kernel_type(1), normalise(0), gamma(0.001), constant(1.0), order(2.0) {}
 	explicit PCA(MatrixXd& d) : components(2), kernel_type(1), normalise(0), gamma(0.001), constant(1.0), order(2.0) {X = d;}
+	void load_data(const char* data, char sep = ',');
+	void set_components(const int i){components = i;};
+	void set_kernel(const int i){kernel_type = i;};	
+	void set_normalise(const int i){normalise = i;};
+	void set_gamma(const double i){gamma = i;};
