f74420f9e1ea6643fb17aec5b8c4f9b8fdb95bcf
CheeseCrap <122087526+CheeseCrap@users.noreply.github.com>
2023-04-16 10:10:56 +0000

    fixing search

--- src/plot.R
+++ src/plot.R
@@ -4,3 +4,7 @@ filename <- "wikipedia_data.png"
 data <- read.table("data/wikipedia.data", header=FALSE, sep="," ,comment.char="#")
 png(filename)
 plot(data$V1[1:156],data$V2[1:156],col='red',xlab="X",ylab="Y",main="Original Data")
+points(data$V1[1:30],data$V2[1:30],col='red')
+points(data$V1[31:90],data$V2[31:90],col='green')
+points(data$V1[91:156],data$V2[91:156],col='blue')
+x <-dev.off()
