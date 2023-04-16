b0d0b76019f554dd7cd73a96164d309773a6ff17
CheeseCrap <122087526+CheeseCrap@users.noreply.github.com>
2023-04-16 14:19:13 +0000

    better error logging

--- src/plot.R
+++ src/plot.R
@@ -8,3 +8,6 @@ points(data$V1[1:30],data$V2[1:30],col='red')
 points(data$V1[31:90],data$V2[31:90],col='green')
 points(data$V1[91:156],data$V2[91:156],col='blue')
 x <-dev.off()
+
+filename <- "wikipedia_RBF_transformed.png"
+data <- read.table("data/transformed_RBF_data.csv", header=FALSE, sep="," ,comment.char="#")
